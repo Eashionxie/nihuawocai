5622b81a5f48fc61816b9cc315a30a566c6b1049
Eashionxie <37648634+Eashionxie@users.noreply.github.com>
2020-07-20 22:27:36 +0800

    添加canvas事件

--- src/views/game/index.vue
+++ src/views/game/index.vue
@@ -1,14 +1,43 @@
 <template>
     <div class="page">
-        <canvas class="game-canvas" @touchstart="touchstart"></canvas>
+        <canvas ref="canvas" class="game-canvas" @touchstart="touchstart" @touchmove="touchmove"></canvas>
     </div>
 </template>
 
 <script>
 export default {
+    data () {
+        return {
+            canvas: '',
+            startX: '',
+            startY: '',
+            stageInfo: ''
+        }
+    },
+    mounted () {
+        const CANVAS = this.$refs.canvas
+        CANVAS.width = CANVAS.offsetWidth
+        CANVAS.height = CANVAS.offsetHeight
+        this.canvas = CANVAS.getContext('2d')
+        this.stageInfo = CANVAS.getBoundingClientRect()
+        this.canvas.lineWidth = 3
+        this.canvas.strokeStyle = "red"
+        this.canvas.lineCap = 'round'
+        this.canvas.lineJoin = 'round'
+    },
     methods: {
-        touchstart () {
-            console.log('start')
+        touchstart (pos) {
+            this.canvas.beginPath()
+            this.startX = pos.touches[0].pageX - this.stageInfo.left,
+            this.startY = pos.touches[0].pageY - this.stageInfo.top
+            this.canvas.moveTo(this.startX, this.startY)
+        },
+        touchmove (pos) {
+            this.canvas.lineTo(
+                pos.touches[0].pageX - this.stageInfo.left,
+                pos.touches[0].pageY - this.stageInfo.top
+            )
+            this.canvas.stroke()
         }
     }
 }
@@ -16,8 +45,10 @@ export default {
 
 <style lang="scss" scoped>
 .game-canvas {
-    width: 100%;
+    width: 96%;
     height: 260px;
+    margin-top: 20px;
     background-color: #fff;
+    border-radius: 8px
 }
 </style>
